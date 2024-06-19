19988a395805331c98560a4bea3a72ad37f7a268
GameDog9988 <alex@grattan.me>
2024-06-19 16:06:46 -0400

    feat: add all other models to Django admin

--- whistlewatchcards/jobboard/admin.py
+++ whistlewatchcards/jobboard/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Club, Assignor, Referee, Job, Application, Assignment
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Club)
+admin.site.register(Assignor)
+admin.site.register(Referee)
+admin.site.register(Job)
+admin.site.register(Application)
+admin.site.register(Assignment)
