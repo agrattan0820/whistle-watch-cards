6ac7bb4e9fb5727a5e4997f0f884fb084e9c032a
GameDog9988 <alex@grattan.me>
2024-06-20 19:51:57 -0400

    feat: create initial urls and index template

--- whistlewatchcards/jobboard/urls.py
+++ whistlewatchcards/jobboard/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("ping", views.current_datetime, name="ping"),
+    path("signin", views.signin, name="signin"),
+    path("create-account", views.create_account, name="create-account"),
+    path("job/<uuid:job_id>", views.job_details, name="job-details"),
+    path("job/<uuid:job_id>/apply", views.job_apply, name="job-apply"),
+]
--- whistlewatchcards/jobboard/views.py
+++ whistlewatchcards/jobboard/views.py
@@ -1,5 +1,29 @@
+import datetime
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the jobboard index.")
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+
+def signin(request):
+    return HttpResponse("Signin page.")
+
+
+def create_account(request):
+    return HttpResponse("Create account page.")
+
+
+def job_details(request):
+    return HttpResponse("Job details page.")
+
+
+def job_apply(request):
+    return HttpResponse("Job apply page.")
