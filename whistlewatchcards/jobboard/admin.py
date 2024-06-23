4d49890df87121a7b91d733dd0e9523f0a61d569
Alexander Grattan <alex@grattan.me>
2024-06-23 16:31:12 -0400

    feat: add AssignorClub model to admin dashboard

--- whistlewatchcards/jobboard/admin.py
+++ whistlewatchcards/jobboard/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Club, Assignor, Referee, Job, Application, Assignment
+from .models import User, Club, Assignor, Referee, Job, Application, Assignment, AssignorClub
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Club)
 admin.site.register(Assignor)
+admin.site.register(AssignorClub)
 admin.site.register(Referee)
 admin.site.register(Job)
 admin.site.register(Application)
